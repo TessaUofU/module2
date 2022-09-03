c28fd1fc1f9af0344e50f68f5cc79c7a00874e6f
TessaUofU <108597090+TessaUofU@users.noreply.github.com>
2022-09-03 15:21:07 -0600

    Add files via upload

---
+++ Quiz2.py
@@ -0,0 +1,14 @@
+# Question 7
+Medication = 5 #Jacks prescription amount
+print(type(Medication))
+
+
+
+# Question 8
+w = 60         #Susan's weight in Kilograms
+h = 1.58       # Susan's height in Meters
+
+BMI = (w / (h**2))
+
+S = round(BMI, 2)
+print("Susan's BMI is:", S)
